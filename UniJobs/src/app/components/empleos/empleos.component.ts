92455b860829679e42c2bb12fa592c3d03f77d67
Francisco <franci.arayag@duocuc.cl>
2021-11-06 18:30:13 -0300

    Empleos

--- UniJobs/src/app/components/empleos/empleos.component.ts
+++ UniJobs/src/app/components/empleos/empleos.component.ts
@@ -6,7 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./empleos.component.scss'],
 })
 export class EmpleosComponent implements OnInit {
-
+  public ocultar1: boolean = false;
+  public ocultar2: boolean = true;
+  accion1(){
+  this.ocultar1 = !this.ocultar1;
+  this.ocultar2 = !this.ocultar2;
+  }
+  private empleo =[
+    {
+    id : '1',
+    title: 'Paseo de mascota',
+    name: 'Juan',
+    imageURL:'../../assets/img/paseomascota.png',
+    comment:'Necesito dar un paseo a mi perro, pero no cuento con tiempo'
+  },
+  {
+    id : '2',
+    title: 'Manicure',
+    name: 'Francisco',
+    imageURL:'../../assets/img/manicure.jpg',
+    comment:'Necesito dar un paseo a mi perro, pero no cuento con tiempo'
+  },
+  {
+    id : '3',
+    title: 'DJ',
+    name: 'Francisco',
+    imageURL:'../../assets/img/job3.jpg',
+    comment:'Necesito dar un paseo a mi perro, pero no cuento con tiempo'
+  }
+] 
   constructor() { }
 
   ngOnInit() {}
