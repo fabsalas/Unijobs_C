889c8a2f5c37fed629ad22ebb38d50348ebfedbb
Francisco <franci.arayag@duocuc.cl>
2021-11-06 20:24:25 -0300

    Perfil básico

--- UniJobs/src/app/components/mi-perfil/mi-perfil.component.ts
+++ UniJobs/src/app/components/mi-perfil/mi-perfil.component.ts
@@ -1,12 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
+import {Component} from '@angular/core';
 
+export interface Vegetable {
+  name: string;
+}
 @Component({
   selector: 'app-mi-perfil',
   templateUrl: './mi-perfil.component.html',
   styleUrls: ['./mi-perfil.component.scss'],
 })
-export class MiPerfilComponent implements OnInit {
 
+export class MiPerfilComponent {
+  vegetables: Vegetable[] = [
+    {name: 'Paseador'},
+    {name: 'Aseo'},
+    {name: 'Cocina'},
+    {name: 'Seguridad'},
+    {name: 'Niñera'},
+  ];
+
+  drop(event: CdkDragDrop<Vegetable[]>) {
+    moveItemInArray(this.vegetables, event.previousIndex, event.currentIndex);
+  }
   constructor() { }
 
   ngOnInit() {}
